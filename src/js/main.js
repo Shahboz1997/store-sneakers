0ec45ddaa5673062f6545f2906fb482dea160d89
Shahboz1997 <sashabilov25@gmail.com>
2025-09-14 22:33:25 +0500

    check conflict

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,62 @@ collectionSvg.forEach((information) =>{
     });
 });
 console.log("hello world");
+// Данные
+const productData = [
+    {
+        id: 0,
+        img: 'img/1.webp',
+        name: 'nike',
+        price: 500,
+        description: 'It is the best of the best',
+    },
+    {
+        id: 1,
+        img: 'img/2.webp',
+        name: 'Nike',
+        price: 600,
+        description: 'It is the best of the best',
+    },
+       {
+        id: 2,
+        img: 'img/1.webp',
+        name: 'nike',
+        price: 500,
+        description: 'It is the best of the best',
+    },
+    {
+        id: 3,
+        img: 'img/2.webp',
+        name: 'Nike',
+        price: 600,
+        description: 'It is the best of the best',
+    },
+       {
+        id: 4,
+        img: 'img/1.webp',
+        name: 'nike',
+        price: 500,
+        description: 'It is the best of the best',
+    },
+    {
+        id: 5,
+        img: 'img/2.webp',
+        name: 'Nike',
+        price: 600,
+        description: 'It is the best of the best',
+    },
+]
+// Функция, которая создаст карточку
+function createCard() {
+    
+}
+
+
+
+//Функция, которая добавит карточки в контейнер
+function addCardToContainer() {
+
+}
+
+
+// При загрузке страницы 
